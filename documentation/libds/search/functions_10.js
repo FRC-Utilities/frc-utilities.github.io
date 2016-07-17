869e6cc6df842743fc2af5ddeb7b332b77c994f0
Alex Spataru <alex_spataru@outlook.com>
2016-07-17 14:38:04 -0500

    Update documentation

--- documentation/libds/search/functions_10.js
+++ documentation/libds/search/functions_10.js
@@ -1,9 +1,7 @@
 var searchData=
 [
-  ['update',['update',['../class_c_r_c32.html#ada7729bd38a502cfc1c256b4c587b8a8',1,'CRC32::update(QByteArray buf)'],['../class_c_r_c32.html#a63ed869ad56e966513e95cac2049eaba',1,'CRC32::update(QByteArray buf, int off, int len)']]],
-  ['updateaddresses',['updateAddresses',['../class_driver_station.html#a2e68dd00997ee3abb8d239c4f616e974',1,'DriverStation::updateAddresses()'],['../class_driver_station.html#a2e20f7d0c8ed0049a45aa4eddf871098',1,'DriverStation::updateAddresses(int unused)']]],
-  ['updateaxis',['updateAxis',['../class_driver_station.html#a81082a639e009d3776530b7c9ff27a84',1,'DriverStation']]],
-  ['updatebutton',['updateButton',['../class_driver_station.html#afe063291b60ff369376364bd97127775',1,'DriverStation']]],
-  ['updatepacketloss',['updatePacketLoss',['../class_driver_station.html#abd3e7ee528d9a3d7a21cb7f661f4aa90',1,'DriverStation']]],
-  ['updatepov',['updatePOV',['../class_driver_station.html#acc95f60d52fa57abb1f1d9c88de92b7e',1,'DriverStation']]]
+  ['team',['team',['../class_d_s___config.html#a2ced3cab98df9a102e090f43ecb97025',1,'DS_Config::team()'],['../class_driver_station.html#a0647be6fd9560adb6fea3a1d50b25ac2',1,'DriverStation::team()']]],
+  ['teamchanged',['teamChanged',['../class_d_s___base.html#ad4df65673c3fabd1f6161804e25d3f5f',1,'DS_Base']]],
+  ['teamstations',['teamStations',['../class_driver_station.html#a7b44ab9fae3f18829654d0ac8e83e62a',1,'DriverStation']]],
+  ['timezone',['timezone',['../class_d_s.html#a533d8902bab58f9dcf5d9772c422b9f6',1,'DS']]]
 ];
